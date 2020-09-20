4e8c7c13c3d4ffb0e4ab8c494b6726e0caa4f15c
Anupam Haldkar <ahdev2020@outlook.com>
2020-09-20 23:29:51 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,6 +1,7 @@
 #  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
 
-consumer_key(API Key) = ''
-consumer_secret(API Secret) = ''
+#API == Consumer
+consumer_key = ''
+consumer_secret = ''
 access_token = ''
 access_token_secret = ''
