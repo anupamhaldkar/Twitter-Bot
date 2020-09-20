b26886f2cf360e8bf56e8092f59bfd8fe4fc6019
Anupam Haldkar <ahdev2020@outlook.com>
2020-09-20 23:28:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Edit this confif file as you like
+# Edit this config file as you like
 
 # This is hastag which Twitter bot will search and retweet. You can edit this with any hastag .For example : '#javascript'
 
